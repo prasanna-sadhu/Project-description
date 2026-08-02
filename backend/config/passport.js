8dcd946afe1e564ca9ebe931b289136f5f500a1b
Prasanna Sadhu <sadhuvenkataprasanna@gmail.com>
2026-08-02 15:13:22 +0530

    update google oauth callback for production

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -14,9 +14,7 @@ clientID: process.env.GOOGLE_CLIENT_ID,
 
 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-callbackURL:
-"http://localhost:5000/api/auth/google/callback"
-
+callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
 },
 
 async(
